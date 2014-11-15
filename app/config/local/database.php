edb005ad1b16dbfe6c038cfdbb6958cef671cb1f
Jeroen Noten <jeroennoten@me.com>
2014-11-15 14:39:48 +0100

    local database configuration

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -20,21 +20,10 @@ return array(
 
 	'connections' => array(
 
-		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
-		),
-
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'homestead',
+			'database' => 'videolyrics',
 			'username' => 'homestead',
 			'password' => 'secret',
 			'charset'  => 'utf8',
