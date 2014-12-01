493d20a8ed84f2deb37e29c62f800e3686e98c51
Kevin Jacobs <kevin91nl@gmail.com>
2014-12-01 18:36:51 +0100

    Added ChartLyrics

--- app/controllers/MusicController.php
+++ app/controllers/MusicController.php
@@ -28,6 +28,18 @@
  *              @SWG\ResponseMessage(code=200, message="A response from the Spotify API."),
  *              @SWG\ResponseMessage(code=500, message="The query could not be executed.")
  *          )
+ *      ),
+ *      @SWG\Api(
+ *          description="Music look-up engine (ChartLyrics proxy).",
+ *          path="/proxy/chartlyrics",
+ *          @SWG\Operation(
+ *              method="GET",
+ *              summary="Proxy to the ChartLyrics API. For example, you can call /SearchLyric?artist=Coldplay&song=Yellow just like you would call it on the ChartLyrics API.",
+ *              nickname="music",
+
+ *              @SWG\ResponseMessage(code=200, message="A response from the ChartLyrics API."),
+ *              @SWG\ResponseMessage(code=500, message="The query could not be executed.")
+ *          )
  *      )
  * )
  */
@@ -46,6 +58,13 @@ class MusicController extends Controller {
     {
         $params = Input::all();
         $API = new APIModel('https://api.spotify.com/v1/', [], ['X-Mashape-Key' => 'M2vWzykqq9mshOXiNMBGQ8fJF1DHp16VbBwjsnyVW3CqW4ZksQ']);
+        $result = $API->call(Request::method(), $path, $params);
+        return Response::make($result, 200);
+    }
+
+    public function lookupChartLyrics_10($path) {
+        $params = Input::all();
+        $API = new APIModel('http://api.chartlyrics.com/apiv1.asmx', [], []);
         $result = $API->call(Request::method(), $path, $params);
         return Response::make($result, 200);
     }
